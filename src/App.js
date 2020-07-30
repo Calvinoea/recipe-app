3593a544c1b95d77bc92825a53893347c2361715
calvinoea <calvinebunamu@gmail.com>
2020-07-30 17:50:41 +0100

    API requests added and recipe information added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -7,7 +7,26 @@ const App = () => {
     const APP_ID = '6c832971';
     const APP_KEY = 'd7902282d8210656c4df0311e41673c3';
 
-    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const [rescipes, setRecipes] = useState([]);
+
+
+    useEffect(() => {
+
+        getRecipes();
+
+    }, [])
+
+
+    const getRecipes = async() => {
+
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        const data = await response.json();
+        setRecipes(data.hits);
+
+
+    }
+
+
 
     return ( <
         div className = "App" >
@@ -18,8 +37,12 @@ const App = () => {
         type = "text/" / >
         <
         button className = "search-button"
-        type = "submit" > Search < /button>  < /
-        form > < /
+        type = "submit" >
+        Search < /button>  < /
+        form >
+
+        <
+        /
         div >
 
     );
---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+3.4 k
+
+const Recipe = () => {
+
+    return ( <
+        div >
+        <
+        h1 > Title < /h1> <
+        p > Calories < /p> <
+        img src = ""
+        alt = "" / >
+        <
+        /div>
+    );
+}
+
+export default Recipe;
